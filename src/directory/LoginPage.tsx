0d2f768e4553387c8789eb0a1ce0903254aaf0d8
ALANN <148600292+RainAllln@users.noreply.github.com>
2025-04-23 20:21:00 +0800

    feat:add basic login page

--- src/directory/LoginPage.tsx
+++ src/directory/LoginPage.tsx
@@ -1,23 +1,45 @@
 import { useNavigate } from "react-router-dom"
+import { Button, Image, Input, Tooltip } from "antd"
+import { EyeInvisibleOutlined, EyeTwoTone, InfoCircleOutlined, KeyOutlined, UserOutlined } from "@ant-design/icons"
+import bg from "@/assets/login.png"
 
 const LoginPage = () => {
   const navigate = useNavigate()
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-4">Login Page</h1>
-      <form className="bg-white p-6 rounded shadow-md w-96">
-        <div className="mb-4">
-          <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
-          <input type="text" id="username" className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-500" />
+    <div className="flex flex-row min-h-screen" style={{
+      backgroundImage: `url(${bg})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      backgroundColor: "rgba(255, 255, 255, 0.5)", // Add a white overlay
+      backgroundBlendMode: "lighten" // Blend the overlay with the image
+    }}>
+      <div className=" flex justify-center items-center w-full">
+        <div className="flex flex-col items-center bg-opacity-90 p-8 rounded w-2/5">
+          <div className="text-4xl font-bold " style={{ fontFamily: 'STCaiyun, 华文彩云' }} >欢迎来到萃豆馆</div>
+          <Input
+            placeholder="Enter your username"
+            prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+            suffix={
+              <Tooltip title="请输入您的账号">
+                <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+              </Tooltip>
+            }
+            className="my-4"
+          />
+          <Input.Password
+            placeholder="input password"
+            prefix={<KeyOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+            iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+            className="my-4"
+          />
+          <div className="flex justify-between w-full mt-2">
+            <Button type="primary">登录</Button>
+            <Button>注册</Button>
+          </div>
         </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-          <input type="password" id="password" className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-500" />
-        </div>
-        {/* <button onSubmit={() => navigate('/')} className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Login</button> */}
-        <span onClick={() => navigate('/')} className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">Login</span>
-      </form>
+      </div>
     </div>
   )
 }
